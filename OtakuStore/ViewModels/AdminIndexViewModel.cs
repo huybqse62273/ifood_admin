60979aec12d607740d8e47bfd1ff228a6d394dfc
quanghuy1103 <38184306+quanghuy1103@users.noreply.github.com>
2018-11-08 17:33:03 +0700

    Update ViewModel

---
+++ OtakuStore/ViewModels/AdminIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OtakuStore.ViewModels
+{
+    public class AdminIndexViewModel
+    {
+        public List<Dish> list { get; set; }
+    }
+}
